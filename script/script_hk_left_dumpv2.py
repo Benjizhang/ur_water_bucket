b515fd922e02c6640f126d699990406b8beaf892
Benjizhang <zzq0551@foxmail.com>
2023-02-27 15:15:19 +0800

    [M] update pkl folder path

--- script/script_hk_left_dumpv2.py
+++ script/script_hk_left_dumpv2.py
@@ -128,6 +128,7 @@ if __name__ == '__main__':
     # folder name
     expFolderName = '/20230225_exphk' # <<<<<<
     NutStorePath = '/home/ur5/Nutstore Files/Nutstore/water_manipulate'
+    DataPKLPath = '/home/ur5/Niu, Yaru/23cm_7_8_9cm'
     dataPath = NutStorePath+expFolderName+'/data'
     figPath = NutStorePath+expFolderName+'/fig'
     bagPath = '/home/ur5/ur5_ws/src/ur_control/logs' #'/home/ur5/rosbag'
@@ -145,9 +146,11 @@ if __name__ == '__main__':
     ## horizonal angle
     hori_angle_rad = -4.251274840994698
 
+    amount_goal_ls = [0.6,0.7,0.8,0.65,0.75]
+
     amount_goal = 0.75
     pos_goal    = 3
-    waterline   = 1 #<<<< serious when change it
+    waterline   = 3 #<<<< serious when change it
     print(f'------ amount_goal: {amount_goal} ------')
     print(f'------ pos_goal:    {pos_goal} ------')
     print(f'------ waterline:   {waterline} ------')
@@ -211,9 +214,9 @@ if __name__ == '__main__':
 
     ## [bucket] generate waypts along bucket path
     bucketVelScale=1.0
-    trajFolderName = '/scaled_trajs_23cm' # '/scaled_trajs_30cm'
+    trajFolderName = '/scaled_trajs_035' # '/scaled_trajs_30cm'
     fileName = '/bucket_amount_goal_'+str(amount_goal)+'_pos_goal_'+str(pos_goal)+'_waterline_'+str(waterline)+'_seed_0_error_*.pkl' # unit: mm
-    file_dir = NutStorePath+trajFolderName+fileName
+    file_dir = DataPKLPath+trajFolderName+fileName
 
     execute = False
     waypts = urGivenPath4(ur_control,file_dir,cur_path_id,oigin_pt,oigin_angle_rad,0,110)
